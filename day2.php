805838731cc3d8aac1c807ba72e64c989b6ec33f
M.shaban <sheboo.radwan@gmail.com>
2023-12-11 16:07:48 +0200

    day2 php lab

---
+++ day2.php
@@ -0,0 +1,18 @@
+
+//
+//echo $lang[0];
+//echo $lang[1];
+//echo $lang[2];
+//echo $lang[3];
+//
+///* Number 01*/
+// $langs = ["php","html","js","css"];
+////Number 02/
+//     foreach($langs as $lang){
+//       echo $lang."<br>";
+//     }
+//     $len = count($langs);
+//   for($i = 0 ;$i < $len ; $i++){
+//        echo $langs[$i]."<br>";
+//    }
+//     $i = 0 ;
